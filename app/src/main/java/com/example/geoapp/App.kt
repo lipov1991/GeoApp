e611a6a532ccf9631e32402a82cc92936cc74e14
KonWys01 <konradwysokinski01@gmail.com>
2023-03-20 19:09:53 +0100

    initial

--- app/src/main/java/com/example/geoapp/App.kt
+++ app/src/main/java/com/example/geoapp/App.kt
@@ -1,7 +1,11 @@
 package com.example.geoapp
 
 import android.app.Application
+import com.example.geoapp.domain.utils.Fingerprint
+import com.example.geoapp.domain.utils.PointFingerprints
 import org.koin.core.context.GlobalContext.startKoin
+import com.example.geoapp.domain.utils.PositioningUtils;
+import com.example.geoapp.domain.utils.LocationHandler;
 
 
 class App : Application() {
@@ -11,5 +15,11 @@ class App : Application() {
         startKoin {
             modules(viewModelsModule, repositoriesModule, utilsModule)
         }
+//        PositioningUtils().locateUserByRouters(listOf(listOf(10.0, 10.0, 10.0)))
+
+        LocationHandler(
+            listOf(Fingerprint("1", -1.0, "1", 0)),
+            listOf(PointFingerprints("1", -1.0, 0))
+        )
     }
 }
--- app/src/main/java/com/example/geoapp/domain/utils/PositioningUtils.kt
+++ app/src/main/java/com/example/geoapp/domain/utils/PositioningUtils.kt
@@ -1,10 +1,59 @@
 package com.example.geoapp.domain.utils
 
+//data class Fingerprint(
+//    val bssid: String,
+//    val signal_level: Double,
+//    val room_name: String,
+//    val direction: Int
+//) {}
+//
+//data class PointFingerprints(val bssid: String, val signal_level: Double, val direction: Int = 0) {}
+//
+//class LocationHandler(
+//    val historicFingerprints: List<Fingerprint>,
+////    val pointsFingerprints: List<PointFingerprints>
+//) {
+//    var grouped_data = hashMapOf<String, Any?>();
+//
+//    fun group_measurements() {
+//        for (measurement in historicFingerprints) {
+//            if (!grouped_data.containsKey(measurement.room_name)) {
+//                grouped_data.put(measurement.room_name, hashMapOf<String, Any?>())
+//            }
+//
+//            if (!grouped_data[measurement.room_name].containsKey(measurement.bssid)) {
+//                grouped_data[measurement.room_name].put(measurement.bssid, mutableListOf<Fingerprint>())
+//            }
+//
+//            grouped_data.get(measurement.room_name).get(measurement.bssid).add(measurement)
+//        }
+//    }
+//
+//    fun get_average_measurement_from_directions(measurements: List<PointFingerprints>): Double {
+//        """
+//            przekazujemy juz pogrupowane fingepritny - np 4 z tym samym pokojem i tym samym routerem
+//        """.trimIndent()
+//        var average_value: Double = 0.0;
+//        for (measurement in measurements) {
+//            average_value += measurement.signal_level;
+//        }
+//        return average_value / measurements.size;
+//    }
+//}
 
 // TODO Tutaj będzie logika związana z pozycjonowaniem użytkownika w budynku
 class PositioningUtils {
 
     fun locateUser() {
         // TODO
+    }
+
+    fun calcPosition(
+        historicFingerprints: List<Fingerprint>,
+        pointsFingerprints: List<PointFingerprints>
+    ) {
+        for (fingerprint in historicFingerprints) {
+            print(fingerprint)
+        }
     }
 }
