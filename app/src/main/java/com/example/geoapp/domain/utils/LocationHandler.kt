b00bf6a21ffb79cafe1abc00ae4db99d54b5c2b7
Damian Szarkowski <dm.szarkowski@gmail.com>
2023-04-04 21:15:17 +0200

    Update LocationHandler.kt

--- app/src/main/java/com/example/geoapp/domain/utils/LocationHandler.kt
+++ app/src/main/java/com/example/geoapp/domain/utils/LocationHandler.kt
@@ -1,5 +1,8 @@
 package com.example.geoapp.domain.utils
 
+import kotlin.math.abs
+import kotlin.math.min
+
 
 data class Router(
     val name: String,
@@ -20,28 +23,22 @@ data class Fingerprint(
 
 data class PointFingerprints(val bssid: String, val signal_level: Double, val direction: Int = 0) {}
 
+data class AverageSignalStrength(val bssid:String, val average_signal_level: Double) {}
+
+data class TempMin(var room_name: String, var min: Double) {}
+
 class LocationHandler(
     val historicFingerprints: List<Fingerprint>,
     val pointsFingerprints: List<PointFingerprints>
 ) {
-    var grouped_data = hashMapOf<String, Any?>();
 
-//    fun group_measurements() {
-//        for (measurement in historicFingerprints) {
-//            if (!grouped_data.containsKey(measurement.room_name)) {
-//                grouped_data.put(measurement.room_name, hashMapOf<String, Any?>())
-//            }
-//
-//            if (!grouped_data[measurement.room_name].containsKey(measurement.bssid)) {
-//                grouped_data[measurement.room_name].put(
-//                    measurement.bssid,
-//                    mutableListOf<Fingerprint>()
-//                )
-//            }
-//
-//            grouped_data.get(measurement.room_name).get(measurement.bssid).add(measurement)
-//        }
-//    }
+    //TODO grupowanie pomiarów
+    //Co kazde klikniecie dostajemy pomiary do wszystkich routerów z danego kierunku, np. north
+    //Znalezc pomiary z 4 kierunków dla każdego routera
+    //Wywołać funkcje uśredniania (get_average_measurement_from_directions) dla każdego routera
+    //Utworzyć AverageSignalStrength(nazwa pokoju, średnia wartość do routera z tego pokoju)
+    //Wyliczyć dystans funkcją (calculate_distance) - zmienić na euclidean distance
+    //Zwracamy nazwę pokoju
 
     fun get_average_measurement_from_directions(measurements: List<PointFingerprints>): Double {
         """
@@ -52,5 +49,21 @@ class LocationHandler(
             average_value += measurement.signal_level;
         }
         return average_value / measurements.size;
+    }
+
+    fun calculate_distance(average_measurement: AverageSignalStrength, Rooms: List<Room>): String {
+        var min = TempMin("0", -1.0)
+        for(room in Rooms){
+            for(router in room.routers){
+                if(router.name == average_measurement.bssid){
+                    var temp = abs(average_measurement.average_signal_level - router.signal_strength)
+                    if(temp < min.min){
+                        min.room_name = room.name;
+                        min.min = temp;
+                    }
+                }
+            }
+        }
+        return min.room_name;
     }
 }
