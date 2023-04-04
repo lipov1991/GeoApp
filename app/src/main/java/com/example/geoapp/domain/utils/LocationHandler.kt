b485ff09092d486df277192f819b22be0a7ce505
KonWys01 <konradwysokinski01@gmail.com>
2023-04-04 22:05:24 +0200

    sort, adding_fingerprint, getting_average

--- app/src/main/java/com/example/geoapp/domain/utils/LocationHandler.kt
+++ app/src/main/java/com/example/geoapp/domain/utils/LocationHandler.kt
@@ -1,7 +1,6 @@
 package com.example.geoapp.domain.utils
 
 import kotlin.math.abs
-import kotlin.math.min
 
 
 data class Router(
@@ -31,6 +30,7 @@ class LocationHandler(
     val historicFingerprints: List<Fingerprint>,
     val pointsFingerprints: List<PointFingerprints>
 ) {
+    private var fingerprints: List<Router> = mutableListOf();
 
     //TODO grupowanie pomiarów
     //Co kazde klikniecie dostajemy pomiary do wszystkich routerów z danego kierunku, np. north
@@ -39,6 +39,53 @@ class LocationHandler(
     //Utworzyć AverageSignalStrength(nazwa pokoju, średnia wartość do routera z tego pokoju)
     //Wyliczyć dystans funkcją (calculate_distance) - zmienić na euclidean distance
     //Zwracamy nazwę pokoju
+
+    fun add_fingerprints_from_one_direction(measurements: List<PointFingerprints>) {
+        """
+            dodawanie kolejnych fingerprintów do pełnej listy
+            po nacisnieciu przycisku do listy maja zostac dopisane wszystkie fingerprinty z danego kierunku: north, east, south, west
+            z tego powodu trzeba bedzie tą funkcję wywołać 4 razy - 4 razy nacisnąć przycisk
+        """.trimIndent()
+        for (measurement in measurements) {
+            val router = Router(name = measurement.bssid, signal_strength = measurement.signal_level);
+            this.fingerprints.add(router);
+        }
+    }
+
+    fun sort_fingerprints() {
+        """
+            funckja sortujaca wszystkie fingerprinty routerów.
+            Po jej wykonaniu fingerprinty bedą prezentowac sie nastepujaco:
+            [
+             Router(name='1', 'signal_strength=1),
+             Router(name='1', 'signal_strength=13),
+             Router(name='1', 'signal_strength=14),
+             Router(name='1', 'signal_strength=12),
+             
+             Router(name='2', 'signal_strength=14),
+             Router(name='2', 'signal_strength=17),
+             Router(name='2', 'signal_strength=51),
+             Router(name='2', 'signal_strength=17),
+             
+             Router(name='3', 'signal_strength=41),
+             Router(name='3', 'signal_strength=15),
+             Router(name='3', 'signal_strength=51),
+             Router(name='3', 'signal_strength=14),
+            ]
+        """.trimIndent()
+        this.fingerprints = this.fingerprints.sortedWith(compareBy {it.name});
+    }
+
+    fun get_average_router_signal(routers: List<Router>): Double {
+        """
+            funkcja przyjmuje liste routerów - 4 (po 1 na kazdy kierunek). Następnie zwraca z nich średnią
+        """.trimIndent()
+        var average_value: Double = 0.0;
+        for (router in routers) {
+            average_value += router.signal_strength;
+        }
+        return average_value / routers.size;
+    }
 
     fun get_average_measurement_from_directions(measurements: List<PointFingerprints>): Double {
         """
