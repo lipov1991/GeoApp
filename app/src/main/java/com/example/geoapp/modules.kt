78594131e8c743b5a4f3cce9b1da2aa0c9f5a14c
JuliaStyp <julia.styp@gmail.com>
2023-04-24 21:22:12 +0200

    Signal Database

--- app/src/main/java/com/example/geoapp/modules.kt
+++ app/src/main/java/com/example/geoapp/modules.kt
@@ -1,24 +1,31 @@
 package com.example.geoapp
 
+
 import com.example.geoapp.data.repository.AuthRepository
-import com.example.geoapp.domain.utils.FirebaseUtils
+import com.example.geoapp.data.repository.FingerprintRepository
 import com.example.geoapp.domain.utils.PositioningUtils
 import com.example.geoapp.ui.auth.AuthViewModel
 import com.example.geoapp.ui.map.MapViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import com.example.geoapp.data.repository.db.signal.SignalDatabase
+import com.example.geoapp.ui.fingerprint.FingerprintViewModel
 
 
 val viewModelsModule = module {
-    viewModel { AuthViewModel(FirebaseUtils = get()) }
+    viewModel { AuthViewModel(authRepository = get()) }
+    viewModel { FingerprintViewModel(fingerprintRepository = get()) }
     viewModel { MapViewModel(positioningUtils = get()) }
 }
 
 val repositoriesModule = module {
+    single {
+        val db = SignalDatabase.getDatabase(context = get())
+        FingerprintRepository(db)
+    }
     single { AuthRepository() }
 }
 
 val utilsModule = module {
     single { PositioningUtils() }
-    single { FirebaseUtils()}
 }
--- app/src/main/java/com/example/geoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/geoapp/ui/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.geoapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.geoapp.R
-import com.example.geoapp.databinding.ActivityAuthBinding
 import com.example.geoapp.ui.auth.AuthFragment
+import com.example.geoapp.ui.fingerprint.FingerprintFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
+
+// Tu będziemy decydować, co wyśweitlić jako pierwsze - ekran logowania czy główny ekran (w przypadku aktywnej sesji).
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityAuthBinding
+
+    private val viewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_auth)
-        supportFragmentManager.beginTransaction().add(R.id.fragment_container, AuthFragment())
-            .commit()
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, FingerprintFragment()).commit()
     }
 }
