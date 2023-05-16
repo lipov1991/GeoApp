b881dd70545e887b1549df9ea708ec9c2d6a14fe
Damian Szarkowski <dm.szarkowski@gmail.com>
2023-05-16 13:03:27 +0200

    Small changes, code cleanup

--- app/src/main/java/com/example/geoapp/App.kt
+++ app/src/main/java/com/example/geoapp/App.kt
@@ -1,10 +1,7 @@
 package com.example.geoapp
 
 import android.app.Application
-import com.example.geoapp.domain.utils.Fingerprint
-import com.example.geoapp.domain.utils.PointFingerprints
 import org.koin.core.context.GlobalContext.startKoin
-import com.example.geoapp.domain.utils.LocationHandler
 
 
 class App : Application() {
@@ -14,10 +11,5 @@ class App : Application() {
         startKoin {
             modules(viewModelsModule, repositoriesModule, utilsModule)
         }
-
-        LocationHandler(
-            listOf(Fingerprint("1", -1.0, "1", 0)),
-            listOf(PointFingerprints("1", -1.0, 0))
-        )
     }
 }
