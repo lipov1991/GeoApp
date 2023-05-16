defe7d8606ef3ae04128998cca1863d486430d77
Damian Szarkowski <dm.szarkowski@gmail.com>
2023-05-16 20:57:54 +0200

    code clean

--- app/src/main/java/com/example/geoapp/domain/utils/LocationHandler.kt
+++ app/src/main/java/com/example/geoapp/domain/utils/LocationHandler.kt
@@ -20,11 +20,8 @@ data class PointFingerprints(val bssid: String, val signal_level: Double, val di
 
 data class TempMin(var room_name: String, var min: Double)
 
-class LocationHandler(
-//    private val historicFingerprints: List<Fingerprint>,
-//    pointsFingerprints: List<PointFingerprints>
-) {
-
+class LocationHandler
+{
     private var fingerprints: MutableList<PointFingerprints> = mutableListOf()
     var routers: List<Router> = listOf()
     private val test = TestData()
